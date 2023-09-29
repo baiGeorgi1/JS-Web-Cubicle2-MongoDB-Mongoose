50fc3457848bad1236fbefa7272e05d0fa90fb88
Georgi Georgiev <gogolino1982@gmail.com>
2023-09-29 11:33:53 +0300

    add create-accessory page

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const homeConfig = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
-
+const accessoryController = require('./controllers/accessoryController');
 
 router.use(homeConfig);
 router.use('/cubes', cubeController); // if the path starts with (/cubes)
+router.use('/accessories', accessoryController);
 router.get('*', (req, res) => {
     res.redirect('/404');
 });
