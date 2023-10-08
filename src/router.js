abd94a2e7c2400f79d0b1f6ec2eb2725be3cb9c5
Georgi Georgiev <gogolino1982@gmail.com>
2023-10-08 16:37:50 +0300

    login and register render pages

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const homeConfig = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 const accessoryController = require('./controllers/accessoryController');
+const userController = require('./controllers/userController');
 
 router.use(homeConfig);
 router.use('/cubes', cubeController); // if the path starts with (/cubes)
 router.use('/accessories', accessoryController);
+router.use('/users', userController);
 router.get('*', (req, res) => {
     res.redirect('/404');
 });
