ce508e0d1d48272a935c56257fe55ee9e5f368b5
Georgi Georgiev <gogolino1982@gmail.com>
2023-10-10 20:05:11 +0300

    fix

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -10,6 +10,10 @@ exports.auth = async (req, res, next) => {
 
             const user = await jwt.verify(token, SECRET); // запазваме токена в потр. на req.user
             req.user = user; // оттук изпращаме по веригата на следващите,кой е потребителя  
+            //тук сетваме променливи,които са достъпни през темл.енджини
+            res.locals.user = user; // така можем в темлейтите да достъпваме данните на потреб.
+            res.locals.isAuthenticated = true;
+
             next();
             //ако е изтекъл токена или е невалиден 
         } catch (err) {
@@ -19,4 +23,10 @@ exports.auth = async (req, res, next) => {
     } else {
         next();
     }
+};
+exports.isAuth = (req, res, next) => {
+    if (!req.user) {
+        return res.redirect('/users/login');
+    }
+    next();
 };
